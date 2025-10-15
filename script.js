4d0c57a3d7f10bfc183a648eecf862c15f7e283a
cadagudi-ops <cadagudi@gmail.com>
2025-10-15 08:42:59 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+let imagenes = [
+    "https:                         
+    "https://images4.alphacoders.com/113/1134176.jpg",
+    "https://i.pinimg.com/originals/04/38/67/043867541f2fc38c2ce6c6b92ffefd7f.jpg",
+    "https:https://vignette.wikia.nocookie.net/topstrongest/images/f/f1/Kevin.png/revision/latest?cb=20170325064442                        
+];
+let indice = 0;
+
+document.getElementById("//picsum.photos/200/302"
+];
+let indice = 0;
+
+document.getElementById("imagen").src = imagenes[indice];
+
+function siguiente() {
+    indice++;
+    if (indice >= imagenes.length) {
+        indice = 0;
+    }
+    document.getElementById("function siguiente() {
+    indice++;
+    if (indice >= imagenes.length) {
+        indice = 0;
+    }
+    document.getElementById("imagen").src = imagenes[indice];
+}
+
+function anterior() {
+    indice--;
+    if (indice < 0) {
+        indice = imagenes.length - 1;
+    }
+    document.getElementById("function anterior() {
+    indice--;
+    if (indice < 0) {
+       indice = imagenes.length - 1;
+    }
+    document.getElementById("imagen").src = imagenes[indice];
+}
+
